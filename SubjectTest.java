b9ce72ae96e3e537240b1c0d860593fd4798c94d
AhmedAbdElmotelbAli <104862736+AhmedAbdElmotelbAli@users.noreply.github.com>
2023-05-12 21:24:26 +0300

    Add files via upload

--- SubjectTest.java
+++ SubjectTest.java
@@ -1,3 +1,8 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+import gpaproject.Subject;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -111,4 +116,88 @@ public class SubjectTest {
           int newFullMark = 90;
           subject.setFullMark(newFullMark);
       }
+
+          @Test // set name 
+    public void testSetName2() {
+        // Test valid name
+        Subject subject = new Subject("Math", "MAT101", 100);
+        subject.setName("Science");
+        assertEquals("Science", subject.getName());
+
+        // Test invalid name (null)
+        try {
+            subject.setName(null);
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Invalid name", e.getMessage());
+        }
+
+        // Test invalid name (empty string)
+        try {
+            subject.setName("");
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Invalid name", e.getMessage());
+        }
+
+        // Test invalid name (contains non-letter characters)
+        try {
+            subject.setName("Math1");
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Invalid name", e.getMessage());
+        }
+
+        // Test invalid name (starts with space)
+        try {
+            subject.setName(" Science");
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Invalid name", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testSetCode1() {
+        // Test valid code
+        Subject subject = new Subject("Math", "MAT101", 100);
+        subject.setCode("SCI101");
+        assertEquals("SCI101", subject.getCode());
+
+         // Test valid code with optional 's'
+         Subject subject2 = new Subject ("Science","SCI101s",100);
+         assertEquals ("SCI101s",subject2.getCode());
+
+         // Test invalid code (null)
+         try{
+             subject.setCode(null);
+             fail ("Expected IllegalArgumentException");
+         }catch(IllegalArgumentException e){
+             assertEquals ("Invalid code" ,e.getMessage());
+         }
+
+        // Test invalid code (wrong format)
+        try {
+            subject.setCode("MAT1011");
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Invalid code", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testSetFullMark3() {
+        // Test valid fullMark
+        Subject subject = new Subject("Math", "MAT101", 100);
+        subject.setFullMark(100);
+        assertEquals(100, subject.getFullMark());
+
+         // Test invalid fullMark
+         try{
+             subject.setFullMark(80);
+             fail ("Expected IllegalArgumentException");
+         }catch(IllegalArgumentException e){
+             assertEquals ("Invalid fullMark" ,e.getMessage());
+         }
+    }
 }
