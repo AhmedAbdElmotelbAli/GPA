9403155758a7d22e2bc3c18d5ed28ff3e55c9337
AhmedAbdElmotelbAli <104862736+AhmedAbdElmotelbAli@users.noreply.github.com>
2023-05-20 17:36:43 +0300

    Add files via upload

--- GPACalculator.java
+++ GPACalculator.java
@@ -11,35 +11,36 @@ package gpaproject;
 
 
 public class GPACalculator {
-   
-
+   private int maxMark = 100;
+    private int minMark = 0;
+    private int minAPlus = 97;
+    private int minA = 93;
+    private int minAmu = 89;
+    private int minBPlus = 84;
+    private int minB = 80;
+    private int minBmu = 76;
+    private int minCPlus = 73;
+    private int minC = 70;
+    private int minCmu = 67;
+    private int minD = 60;
+    private int minDplus = 64;
     public String getLetterGrade(int activitiesMark, int oralMark, int midtermMark, int finalMark) {
         int totalMark = activitiesMark + oralMark + midtermMark + finalMark;
-        
-        if (totalMark >= 97) {
-            return "A+";
-        } else if (totalMark >= 93) {
-            return "A";
-        } else if (totalMark >= 89) {
-            return "A-";
-        } else if (totalMark >= 84) {
-            return "B+";
-        } else if (totalMark >= 80) {
-            return "B";
-        } else if (totalMark >= 76) {
-            return "B-";
-        } else if (totalMark >= 73) {
-            return "C+";
-        } else if (totalMark >= 70) {
-            return "C";
-        } else if (totalMark >= 67) {
-            return "C-";
-        } else if (totalMark >= 64) {
-            return "D+";
-        } else if (totalMark >= 60) {
-            return "D";
-        } else {
-            return "F";
+        if (totalMark <= maxMark && totalMark >= minMark) {
+            if (totalMark >= minAPlus) {     return "A+";
+            } else if (totalMark >= minA) { return "A";
+            } else if (totalMark >= minAmu) {return "A-";
+            } else if (totalMark >= minBPlus) {return "B+";
+            } else if (totalMark >= minB) {return "B";
+            } else if (totalMark >= minBmu) {return "B-";
+            } else if (totalMark >= minCPlus) {return "C+";
+            } else if (totalMark >= minC) {return "C";
+            } else if (totalMark >= minCmu) {return "C-";
+            } else if (totalMark >= minDplus) {return "D+";
+            } else if (totalMark >= minD) {return "D";
+            } else { return "F";
+            }
+        } else { return "error";
         }
     }
-}
+} 
